2e29c0eae51435364d8e2c0cea5036b51d101eed
Dmitry Shtukenberg <dshtuk@mail.ru>
2018-11-12 03:26:15 +0300

    2011, Dima&Vova square

---
+++ other/divs.hs
@@ -0,0 +1,19 @@
+is_prime x = length [t | t<-[2..x `div` 2+1], x `mod` t == 0] == 0
+primes = 2 : [x | x<-[2..],is_prime x]
+
+plog a x | x `mod` a == 0 = 1 + plog a (x `div` a)
+plog a x = 0
+
+divisors 1 pri = []
+divisors x (p:pri) | x `mod` p == 0 = c : divisors (x `div` (p^c)) pri
+        where c = plog p x
+divisors x (_:pri) = 0 : divisors x pri
+
+fact n = product [1..n]
+nearest n = [t | t <- [n+1..], fact n `mod` t /= 0, sum (divisors t primes) > 1] !! 0
+
+main = do
+   print $ take 20 primes
+   print $ divisors 5 primes
+
+   print $ [(k,nearest k,divisors (nearest k) primes)| k <- [1..300]]
